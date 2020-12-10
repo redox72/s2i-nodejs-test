12594eb1adfa4d59f61515c10211385e29bc722b
Ido Faigenboim <ifaigenb@ifaigenb.remote.csb>
2020-12-10 18:18:47 +0200

    show to guy

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Guy is the king');
 });
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
-  console.log("Auto s2i?");
+  console.log("the server is running");
 });
 
