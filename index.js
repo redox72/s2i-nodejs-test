09f45506c1aa9eb1e33c221d91ff64a8a7d887fa
Ido Faigenboim <ifaigenb@ifaigenb.remote.csb>
2020-12-10 18:10:48 +0200

    changed index

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
-  console.log("The configed changes");
+  console.log("Auto s2i?");
 });
 
